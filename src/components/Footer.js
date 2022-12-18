0599d5a6dfc7f1f9ae86ee61900bba0c730c5bd3
mikASho <platon11.90@mail.ru>
2022-12-18 18:51:57 +0300

    Start

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import './Footer.scss';
+export default class Footer extends Component {
+  render() {
+    return (
+      <div>Footer</div>
+    )
+  }
+}
