94ba746a5079cf54753a91b231cb3d81b56c2f3f
mikASho <platon11.90@mail.ru>
2022-12-20 01:21:33 +0300

    Main finish

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -7,6 +7,9 @@ import icon_adv_1 from './img/icon_adv_1.png';
 import icon_adv_2 from './img/icon_adv_2.png';
 import icon_adv_3 from './img/icon_adv_3.png';
 import icon_adv_4 from './img/icon_adv_4.png';
+import icon_deal from './img/deal.png';
+import { Button, Form } from 'react-bootstrap';
+
 export default class Main extends Component {
   render() {
     return (
@@ -42,10 +45,36 @@ export default class Main extends Component {
                   <p style={{position: "relative", top: 10, left: 500, width: 200, height: 27, color: "white"}}>sed do eiusmod tempor incididunt</p>
                   <p style={{position: "relative", top: -30, left: 740, width: 200, height: 27, color: "white"}}>sed do eiusmod tempor incididunt</p>
                 </div>
+              </section>
 
+              <section>
+                <div>
+                  <p style={{fontWeight: 600,fontSize: 30, color: "white", marginLeft:400}}>Lorem ipsum</p>
+                  <p style={{width: 836, height: 170, fontWeight: 400,fontSize: 20, color: "white", marginTop: 10, marginLeft: 62}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  <img src={icon_deal} alt='icon' style={{position:"relative", left: 700, marginTop: -30}}/>
+                </div>
+              </section>
+
+              <section style={{position: "relative", width: 960, height: 300, background: "#161614"}}>
+                <p style={{position: "relative", left: 350, top: 20, fontWeight: 400,fontSize: 20, color: "white"}}>Lorem ipsum dolor sit amet?</p>
+                  <div style={{width: 435, position: "relative", left: 500, top: 50}}>
+                      <Form.Control  
+                        placeholder='Введите ваш e-mail..'
+                        style={{height: 61}}
+                      />
+                      <Form.Control 
+                        style={{height: 61}}
+                        className='mt-10'
+                        placeholder='Вопрос'
+                      />
+                      <Button className='mt-10' variant="outline-light">
+                        Отправить
+                      </Button>       
+                  </div>
+                  <p style={{position: "relative", width: 450, height: 170, fontWeight: 400,fontSize: 20, color: "white", top: -120, left: 30}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet.</p>
               </section>
             </main>
-
+            <footer style={{fontWeight: 800,fontSize: 220, color: "white", position: "relative", width: 960,heigth: 200, left: 500}}>FOOTER</footer>
         </div>
     )
   }
