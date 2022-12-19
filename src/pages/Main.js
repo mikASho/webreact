f2e5ad773c2dc92e814246812385575d168d5e4b
mikASho <platon11.90@mail.ru>
2022-12-19 23:45:28 +0300

    main

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -11,19 +11,29 @@ export default class Main extends Component {
               <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </h1>
             </header>
 
-            <div className='content'>
-              <img src={icon_1} alt='icon' style={{position:"relative", top: "96px", left: 516}}/>
-              <p style={{ position: "relative", top: -150, left: 180, color:"white", fontSize: 20, fontWeight: 800}}>Lorem</p>
-              <p style={{ position: "relative",left: 52, top: -100, width: 396, height: 108, color: "white", fontSize: 20, fontWeight: 600}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                  <div style={{ width: 960, height: 390, backgroundColor: "blue"}}>
-                    <img src={icon_2} alt='icon' style={{position:"relative", top: 40, marginLeft: 43}}/>
-                    <p style={{ position: "relative", top: -180, left: 650, color:"white", fontSize: 20, fontWeight: 800}}>Lorem</p>
-                    <p style={{ position: "relative",left: 510, top: -180, width: 396, height: 108, color: "white", fontSize: 20, fontWeight: 600}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                  </div>     
-              <img src={icon_3} alt='icon' style={{position:"relative", top: 30, left: 516}}/>
-              <p style={{ position: "relative", top: -150, left: 180, color:"white", fontSize: 20, fontWeight: 800}}>Lorem</p>
-              <p style={{ position: "relative",top: -150, left: 52, width: 396, height: 108, color: "white", fontSize: 20, fontWeight: 600}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div>
+            <main>
+              <section>
+                    <img src={icon_1} alt='icon' style={{position:"relative", top: "96px", left: 516}}/>
+                  <p style={{ position: "relative", top: -150, left: 180, color:"white", fontSize: 20, fontWeight: 800}}>Lorem</p>
+                  <p style={{ position: "relative",left: 52, top: -100, width: 396, height: 108, color: "white", fontSize: 20, fontWeight: 600}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                      <div style={{ width: 960, height: 390, backgroundColor: "#161614"}}>
+                        <img src={icon_2} alt='icon' style={{position:"relative", top: 40, marginLeft: 43}}/>
+                        <p style={{ position: "relative", top: -180, left: 650, color:"white", fontSize: 20, fontWeight: 800}}>Lorem</p>
+                        <p style={{ position: "relative", left: 510, top: -180, width: 396, height: 108, color: "white", fontSize: 20, fontWeight: 600}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                      </div>     
+                  <img src={icon_3} alt='icon' style={{position:"relative", top: 30, left: 516}}/>
+                  <p style={{ position: "relative", top: -150, left: 180, color:"white", fontSize: 20, fontWeight: 800}}>Lorem</p>
+                  <p style={{ position: "relative",top: -150, left: 52, width: 396, height: 108, color: "white", fontSize: 20, fontWeight: 600}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </section>
+              
+              <section style={{ position: "relative", top: -30}}>
+                <div className='advantages'>
+
+                </div>
+
+              </section>
+            </main>
+
         </div>
     )
   }
